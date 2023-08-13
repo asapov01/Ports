7f5f7785c2df44c8e59ebbc0a4e1d114c361af1f
Asapov <137000528+asapov01@users.noreply.github.com>
2023-08-13 09:28:23 +0300

    Update and rename Port.sh to Ports.sh

--- Ports.sh
+++ Ports.sh
@@ -8,6 +8,10 @@ function printGreen {
     echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
+function printRed {
+  echo -e "\e[1m\e[31m${1}\e[0m"
+}
+
 function check {
     while true; do
         printGreen "Виберіть, що ви хочете переглянути:"
@@ -17,9 +21,9 @@ function check {
 
         if [[ $choice == "1" ]]; then
             printGreen "В 3 стовбці ви бачите перелік зайнятих портів на вашому сервері."
-            printGreen "Наприклад::::30333 -використовується порт 30333"
-            printGreen "Наприклад::::9090 - використовується порт 9090"
-            printGreen "127.0.0.1:26657 - використовується порт 26657"
+            printRed "Наприклад::::30333 -використовується порт 30333"
+            printRed "Наприклад::::9090 - використовується порт 9090"
+            printRed "Наприклад 127.0.0.1:26657 - використовується порт 26657"
             netstat -tuln
         elif [[ $choice == "2" ]]; then
             break

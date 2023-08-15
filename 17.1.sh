4a09cfc973945894a85ec7938fb755f93862a25f
Asapov <137000528+asapov01@users.noreply.github.com>
2023-08-15 16:15:17 +0300

    Update and rename 17.1.sh to 17.2.sh

--- 17.1.sh
+++
@@ -1,6 +0,0 @@
-echo -n "2  - 9190, 9191, 27658, 27657, 27656, 6160, 27660, 1417"
-            if is_port_taken 9190 || is_port_taken 9191 || is_port_taken 27658 || is_port_taken 27657 || is_port_taken 27656 || is_port_taken 6160 || is_port_taken 27660 || is_port_taken 1417; then
-                echo -e "\e[1m\e[31m9190,9191,27658,27657,27656,6160,27660,1417\e[0m"
-            else
-                echo "2  - 9190, 9191, 27658, 27657, 27656, 6160, 27660, 1417"
-            fi

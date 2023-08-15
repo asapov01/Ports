2753cffd90c9d742b58448d385df608aa290e3fb
Asapov <137000528+asapov01@users.noreply.github.com>
2023-08-15 22:28:32 +0300

    Update and rename 17.7.sh to 17.8.sh

--- 17.8.sh
+++ 17.8.sh
@@ -15,9 +15,9 @@ function printRed {
 function is_port_taken {
     local port=$1
     if ss -tuln | awk -v p=":${port} " '$0 ~ p {exit 1}'; then
-        return 0
+        return 1  
     else
-        return 1
+        return 0  
     fi
 }
 

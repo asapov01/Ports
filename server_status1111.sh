42f885f28d47e6d61f69195cf78a78c78c009d64
Asapov <137000528+asapov01@users.noreply.github.com>
2024-07-05 11:27:16 +0300

    Update and rename server_status11.sh to server_status1111.sh

--- server_status1111.sh
+++ server_status1111.sh
@@ -13,13 +13,23 @@ else
     go_status="не встановлено"
 fi
 
+# Перевірка наявності screen
+if screen -version &> /dev/null; then
+    screen_status="встановлено"
+else
+    screen_status="не встановлено"
+fi
+
 # Кольори
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # Без кольору
 
 # Виведення інформації
+echo ""
 echo -e "${GREEN}На вашому сервері доступно:${NC}"
+echo ""
 echo -e "${GREEN}SSD доступно: ${RED}$ssd_available${NC}"
 echo -e "${GREEN}RAM доступно: ${RED}$ram_available${NC}"
 echo -e "${GREEN}Мова програмування GO: ${RED}$go_status${NC}"
+echo -e "${GREEN}Утиліта screen: ${RED}$screen_status${NC}"

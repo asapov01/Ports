e3178b1a2d6f5917f73d31ca49343ee05baf7466
Asapov <137000528+asapov01@users.noreply.github.com>
2024-07-05 11:23:08 +0300

    Update and rename server_status.sh to server_status1.sh

--- server_status1.sh
+++ server_status1.sh
@@ -6,11 +6,15 @@ ssd_available=$(df -h / | grep '/' | awk '{print $4}')
 # Отримання інформації про доступну пам'ять RAM
 ram_available=$(free -h | grep Mem | awk '{print $7}')
 
-# Отримання інформації про використання CPU
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+# Перевірка наявності Go
+if go_version=$(go version 2>/dev/null); then
+    go_status="встановлено ($go_version)"
+else
+    go_status="не встановлено"
+fi
 
 # Виведення інформації
 echo "На вашому сервері доступно:"
-echo "SSD: $ssd_available"
-echo "RAM: $ram_available"
-echo "CPU: $cpu_usage"
+echo "SSD доступно: $ssd_available"
+echo "RAM доступно: $ram_available"
+echo "Мова програмування GO: $go_status"
